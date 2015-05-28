1f75a835bec60cfe87418d1aaebe59344fd78175
Dafydd Francis <dafydd.francis@andwhathave.eu>
2015-05-28 11:03:09 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'Where to download it.',
     'author_email': 'dafydd.francis@andwhathave.eu',
     'version': '0.1',
-    'install_requires': ['nose', 'pyyaml'],
+    'install_requires': ['nose', 'pyyaml', 'python-dateutil'],
     'packages': ['business'],
     'package_data': {
         'business': ['data/*.yml'],
